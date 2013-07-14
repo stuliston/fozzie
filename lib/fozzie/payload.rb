bc0fa711238df8600abe198a9ec822bb759c120f
Marc Watts <marcky.sharky@googlemail.com>
2013-07-14 18:03:12 +0100

    Syntax changes for Payload.

--- lib/fozzie/payload.rb
+++ lib/fozzie/payload.rb
@@ -22,8 +22,8 @@ module Fozzie
       raise ArgumentError, "bucket required" if @args[:bucket].to_s.empty?
 
       buck = [@args[:bucket]].flatten.compact.collect(&:to_s).join(DELIMETER).downcase
-      buck = buck.gsub(RESERVED_CHARS_REGEX, RESERVED_CHARS_REPLACEMENT)
-      buck = [Fozzie.c.data_prefix, buck].compact.join(DELIMETER)
+      buck = clean_generated_string(buck)
+      buck = [prefix, buck].compact.reject(&:empty?).join(DELIMETER)
 
       buck 
     end
@@ -39,6 +39,10 @@ module Fozzie
     def sample_rate
       '@%s' % @args[:sample_rate].to_s
     end
+    
+    def prefix
+      clean_generated_string(Fozzie.c.prefix.compact.join(DELIMETER).downcase)
+    end
 
     def to_s
       ["#{bucket}:#{value}", type, (sampled? ? sample_rate : nil)].compact.join('|')
@@ -46,6 +50,12 @@ module Fozzie
 
     def sampled?
       @sampled = (@args[:sample_rate].to_i < 1 and rand > @args[:sample_rate].to_i)
+    end
+    
+    private
+    
+    def clean_generated_string(str)
+      str.gsub(RESERVED_CHARS_REGEX, RESERVED_CHARS_REPLACEMENT)
     end
 
   end
