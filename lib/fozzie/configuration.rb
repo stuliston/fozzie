d0d2ecda1dc3ea61369569f769d2687903563641
Marc Watts <marcky.sharky@googlemail.com>
2013-07-14 18:00:59 +0100

    Refactor configuration class.

--- lib/fozzie/configuration.rb
+++ lib/fozzie/configuration.rb
@@ -1,60 +1,33 @@
-require 'yaml'
-require 'facets/hash/symbolize_keys'
-require 'sys/uname'
-require 'timeout'
+require "yaml"
+require "facets/hash/symbolize_keys"
+require "timeout"
+require "resolv"
 
 module Fozzie
 
   # Fozzie configuration allows assignment of global properties
   # that will be used within the Fozzie codebase.
-
   class Configuration
-    include Sys
-    extend Forwardable
 
-    def_delegators :delimeter, :safe_separator
-
-    attr_accessor :env, :config_path, :host, :port, :appname, :namespaces,
-      :timeout, :monitor_classes, :sniff_envs, :ignore_prefix, :prefix
+    attr_accessor :env, :host, :host_ip, :port, :namespaces,
+      :timeout, :monitor_classes, :sniff_envs, :prefix
 
     def initialize(args = {})
       merge_and_assign_config(args)
-      self.origin_name
     end
     
-    def host_ip
-      @host_ip ||= Resolv.getaddress(Fozzie.c.host)
-    end
-
-    def disable_prefix
-      @ignore_prefix = true
-    end
-
-    # Returns the prefix for any stat requested to be registered
-    def data_prefix
-      return nil #if @ignore_prefix
-      return @data_prefix if @data_prefix
-
-      escaped_prefix_with_dynamically_resolved_parts = prefix.map do |part|
-        resolved_part = (part.kind_of?(Symbol) && self.respond_to?(part) ? self.send(part) : part.to_s)
-        escaped_resolved_part = resolved_part.gsub(delimeter, safe_separator)
-        escaped_resolved_part == "" ? nil : escaped_resolved_part
-      end.compact
-
-      @data_prefix = if escaped_prefix_with_dynamically_resolved_parts.any?
-                       escaped_prefix_with_dynamically_resolved_parts.join(delimeter).strip
-                     else
-                       nil
-                     end
-    end
-
-    # Returns the origin name of the current machine to register the stat against
-    def origin_name
-      @origin_name ||= Uname.nodename
+    def host=(val)
+      @host     = val
+      @host_ip  = Resolv.getaddress(@host) rescue nil
+      @host
     end
 
     def sniff?
       self.sniff_envs.collect(&:to_sym).include?(self.env.to_sym)
+    end
+
+    def load(filepath)
+      assign_config(config_from_yaml(filepath))
     end
 
     private
@@ -62,40 +35,44 @@ module Fozzie
     # Handle the merging of the given configuaration, and the default config.
     def merge_and_assign_config(args = {})
       arg = self.class.default_configuration.merge(args.symbolize_keys)
-      arg.merge!(config_from_yaml(arg))
-      arg.each {|a,v| self.send("#{a}=", v) if self.respond_to?(a.to_sym) }
+      arg.merge!(config_from_yaml(full_config_path))
+      assign_config(arg)
+    end
+    
+    def assign_config(args = {})
+      args.each {|a,v| self.send("#{a}=", v) if self.respond_to?(a.to_sym) }
 
-      arg
+      args
     end
 
     # Default configuration settings
     def self.default_configuration
       {
-        :host            => '127.0.0.1',
-        :prefix          => [:appname, :origin_name, :env],
-        :port            => 8125,
-        :config_path     => '',
-        :env             => (ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'),
-        :appname         => '',
+        :env             => (ENV['FOZZIE_ENV'] || 'development'),
+        :host            => (ENV['FOZZIE_HOST'] || '127.0.0.1'),
+        :port            => (ENV['FOZZIE_PORT'] || 8125).to_i,
         :namespaces      => %w{Stats S Statistics Warehouse},
-        :timeout         => 0.5,
+        :timeout         => (ENV['FOZZIE_TIMEOUT'] || 0.5).to_f,
         :monitor_classes => [],
-        :sniff_envs      => [:development, :staging, :production],
-        :ignore_prefix   => false
+        :sniff_envs      => [:development, :staging, :production].collect(&:to_sym),
+        :prefix          => []
       }.dup
     end
 
     # Loads the configuration from YAML, if possible
-    def config_from_yaml(args)
-      fp = full_config_path(args[:config_path])
-      return {} unless File.exists?(fp)
-      cnf = YAML.load(File.open(fp))[args[:env]]
-      (cnf.kind_of?(Hash)) ? cnf.symbolize_keys : {}
+    def config_from_yaml(path)
+      return {} unless File.exists?(path)
+      
+      cnf = YAML.load(File.open(path))
+      cnf = cnf.kind_of?(Hash) ? cnf.symbolize_keys : {}
+      cnf = cnf[env.to_sym] unless env.nil? and cnf[env.to_sym].nil?
+
+      cnf
     end
 
-    # Returns the absolute file path for the Fozzie configuration, relative to the given path
-    def full_config_path(path)
-      File.expand_path('config/fozzie.yml', path)
+    # Returns the absolute file path for the Fozzie configuration
+    def full_config_path
+      File.expand_path('./config/fozzie.yml')
     end
 
   end
--- spec/lib/fozzie/configuration_spec.rb
+++ spec/lib/fozzie/configuration_spec.rb
@@ -1,78 +1,109 @@
 require 'spec_helper'
-require 'resolv'
 
 describe Fozzie::Configuration do
-  it "#host" do
-    subject.host.should be_kind_of(String)
+  let(:mock_env) { Hash.new(nil) }
+
+  before(:each) { stub_const("ENV", mock_env) }
+
+  describe "#env" do
+    
+    it "loads value from ENV" do
+      mock_env['FOZZIE_ENV'] = "ci"
+      subject.env.should eq('ci')
+    end
+    
+    it "defaults env to development" do
+      subject.env.should eq("development")
+    end
   end
 
-  it "#port" do
-    subject.port.should be_kind_of(Fixnum)
+  describe "#host" do
+    
+    it "@host_ip" do
+      subject.host_ip.should eq("127.0.0.1")
+    end
+    
+    it "loads value from ENV" do
+      mock_env['FOZZIE_HOST'] = "foobar.com"
+      subject.host.should eq("foobar.com")
+    end
+    
+    it "defaults to 127.0.0.1" do
+      subject.host.should eq("127.0.0.1")
+    end
   end
 
-  it "attempts to load configuration from yaml" do
-    c = Fozzie::Configuration.new({
-                                    env:         'test',
-                                    config_path: 'spec/'
-                                  })
-    c.stub(:origin_name => "")
-    c.host.should eq '1.1.1.1'
-    c.port.should eq 9876
-    c.appname.should eq 'fozzie'
-    #c.data_prefix.should eq "fozzie#{c.safe_separator}test"
-  end
+  describe "#port" do
 
-  it "defaults env" do
-    subject.env.should eq 'test'
-  end
-
-  #describe "#adapter" do
-  #  it "throw error on incorrect assignment" do
-  #    -> { Fozzie::Configuration.new({:env => 'test', :adapter => 'foo'}) }.should raise_error(Fozzie::AdapterMissing)
-  #  end
-
-  #  it "defaults adapter to Statsd" do
-  #    subject.adapter.should be_kind_of(Fozzie::Adapter::Statsd)
-  #  end
-  #end
-
-  #describe "#prefix and #data_prefix" do
-  #  it "creates a #data_prefix" do
-  #    subject.stub(:origin_name => "")
-  #    subject.data_prefix.should eq 'test'
-  #  end
-
-  #  it "creates a #data_prefix with appname when set" do
-  #    subject.stub(:origin_name => "")
-  #    subject.appname = 'astoria'
-  #    subject.data_prefix.should eq 'astoria.test'
-  #  end
-
-  #  it "creates a #data_prefix with origin" do
-  #    subject.appname = 'astoria'
-  #    subject.data_prefix.should match /^astoria\.(\S+)\.test$/
-  #  end
-
-  #  it "allows dynamic assignment of #prefix to derive #data_prefix" do
-  #    subject.prefix = [:foo, :bar, :car]
-  #    subject.data_prefix.should eq 'foo.bar.car'
-  #  end
-
-  #  it "allows dynamic injection of value to prefix" do
-  #    subject.stub(:origin_name => "")
-  #    subject.prefix << 'git-sha-1234'
-  #    subject.data_prefix.should eq 'test.git-sha-1234'
-  #  end
-  #end
-
-  it "handles missing configuration namespace" do
-    proc { Fozzie::Configuration.new({:env => 'blbala', :config_path => 'spec/'}) }.should_not raise_error
+    it "loads value from ENV" do
+      mock_env['FOZZIE_PORT'] = "123"
+      subject.port.should eq(123)
+    end
+    
+    it "defaults to 8125" do
+      subject.port.should eq(8125)
+    end
   end
 
   it "#namespaces" do
     subject.namespaces.should be_kind_of(Array)
     subject.namespaces.should include("Stats")
     subject.namespaces.should include("S")
+    subject.namespaces.should include("Statistics")
+    subject.namespaces.should include("Warehouse")
+  end
+
+  describe "#timeout" do
+
+    it "loads value from ENV" do
+      mock_env['FOZZIE_TIMEOUT'] = "0.75"
+      subject.timeout.should eq(0.75)
+    end
+    
+    it "defaults to 0.5" do
+      subject.timeout.should be_kind_of(Float)
+    end
+  end
+  
+  describe "#monitor_classes" do
+    
+    it "defaults to empty Array" do
+      subject.monitor_classes.should eq([])
+    end
+  end
+  
+  describe "#sniff_envs" do
+    
+    it "defaults to populated Array" do
+      subject.sniff_envs.should eq([:development, :staging, :production])
+    end
+  end
+  
+  describe "#load" do
+
+    it "attempts to load configuration from yaml" do
+      subject.tap do |s|
+        s.env = "test"
+        s.load('./spec/config/fozzie.yml')
+      end
+
+      subject.host.should eq('1.1.1.1')
+      subject.port.should eq(9876)
+    end
+  end
+  
+  describe "#prefix" do
+
+    it "allows dynamic injection of value to prefix" do
+      subject.prefix << 'git-sha-1234'
+      subject.prefix.should eq(['git-sha-1234'])
+    end
+  end
+  
+  it "handles missing configuration namespace" do
+    proc { 
+      Fozzie::Configuration.new({:env => 'blbala'}) 
+    }.should_not raise_error
   end
 
   describe "#sniff?" do
@@ -92,7 +123,7 @@ describe Fozzie::Configuration do
     end
   end
 
-  describe "#sniff_envs allows configuration for #sniff?" do
+  describe "#sniff_envs sets configuration for #sniff?" do
     let!(:sniff_envs) { subject.stub(:sniff_envs => ['test']) }
 
     it "scopes to return false" do
@@ -104,14 +135,5 @@ describe Fozzie::Configuration do
       subject.stub(:env => "test")
       subject.sniff?.should be_true
     end
-
   end
-
-  describe "ignoring prefix" do
-    it "does not use prefix when set to ignore" do
-      subject.disable_prefix
-      subject.ignore_prefix.should eq(true)
-    end
-  end
-
 end
